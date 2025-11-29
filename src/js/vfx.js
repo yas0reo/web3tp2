80d69bc4e0af9ce19fdd90c17592a793c406151f
yasmine <yass.onekeo@gmail.com>
2025-11-29 00:44:06 -0500

    chart time

--- src/js/vfx.js
+++ src/js/vfx.js
@@ -1,7 +1,7 @@
 import { VFX } from "https://esm.sh/@vfx-js/core";
 
 const vfx = new VFX();
-document.querySelectorAll("NingNingImg").forEach((target) => {
+document.querySelectorAll("img").forEach((target) => {
   vfx.add(target, {
     shader: "glitch",
     overflow: 10,
---
+++ src/js/zdog.js
@@ -0,0 +1,60 @@
+import Zdog from "https://esm.sh/zdog";
+
+var yellow = "#ED0";
+var blue = "#127ac4";
+var eggplant = "#636";
+const TAU = Zdog.TAU;
+
+var illo = new Zdog.Illustration({
+  element: ".zdog-canvas",
+  dragRotate: true,
+});
+
+var eyeGroup = new Zdog.Group({
+  addTo: illo,
+  translate: { z: 20 },
+});
+// eye outline
+new Zdog.Ellipse({
+  addTo: eyeGroup,
+  width: 165,
+  height: 90,
+  stroke: 18,
+  fill: true,
+  color: "black",
+});
+// eye white
+new Zdog.Ellipse({
+  addTo: eyeGroup,
+  width: 160,
+  height: 80,
+  stroke: 8,
+  fill: true,
+  color: "white",
+});
+
+var iris = new Zdog.Ellipse({
+  addTo: eyeGroup,
+  diameter: 70,
+  stroke: 8,
+  fill: true,
+  color: blue,
+});
+// pupil
+iris.copy({
+  diameter: 40,
+  color: eggplant,
+});
+// highlight
+iris.copy({
+  diameter: 30,
+  translate: { x: 15, y: -15 },
+  color: "white",
+});
+
+// 👇
+function animate() {
+  illo.updateRenderGraph();
+  requestAnimationFrame(animate);
+}
+animate();
