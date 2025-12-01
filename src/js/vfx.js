c31b1aaedb9416a9e32deb93b9ff760100c81839
yasmine <yass.onekeo@gmail.com>
2025-12-01 12:08:48 -0500

    bootstrap

--- src/js/vfx.js
+++ src/js/vfx.js
@@ -1,7 +1,7 @@
 import { VFX } from "https://esm.sh/@vfx-js/core";
 
 const vfx = new VFX();
-document.querySelectorAll("img").forEach((target) => {
+document.querySelectorAll(".effect").forEach((target) => {
   vfx.add(target, {
     shader: "glitch",
     overflow: 10,
