442f1003fb3d1d7dba5853a19e7325c5de175ad5
yasmine <yass.onekeo@gmail.com>
2025-12-07 17:33:35 -0500

    audio good

--- src/js/tonejs.js
+++ src/js/tonejs.js
@@ -1,13 +1,13 @@
 import * as Tone from "https://esm.sh/tone@15.1.22";
 
-const synth = new Tone.Synth().toDestination();
-
 const player = new Tone.Player(
   "/assets/audio/BoredningningSolo.mp3"
 ).toDestination();
 
-document.querySelector("button").addEventListener("click", async () => {
-  await Tone.start();
+const btn = document.getElementById("playAudio");
+
+btn.addEventListener("click", async () => {
+  await Tone.start(); // Obligatoire pour débloquer l’audio
 
   Tone.loaded().then(() => {
     player.start();
